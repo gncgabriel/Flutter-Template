f50fcc2cb32ded2d5966499ef89019717ac04afe
Gabriel <gabriell_ca@outlook.com>
2023-03-18 11:12:00 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# app_financeiro
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/app_financeiro/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_financeiro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
